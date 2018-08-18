c6820c4f415f061084aab8c98c64757f08ca8c41
adisorn_tob <adisorn_tob@hotmai.com>
2018-08-19 00:18:23 +0700

    แก้ไขข้อความต้อนรับ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Home
+                    สวัสดี
                 </div>
 
                 <div class="links">
